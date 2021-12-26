505b854bb26cbd1788889ca1e2389e2fc45ab04b
Phil <happy.melodist@gmail.com>
2021-12-26 00:04:24 -0600

    write getRandomCat, fox, and dog functions, completing the functionality of the app

--- app.js
+++ app.js
@@ -3,9 +3,54 @@ Public API Repo
 https://github.com/public-apis/public-apis
 */
 
+// let cat = document.querySelector(".cat");
+// let fox = document.querySelector(".fox");
+// let dog = document.querySelector(".dog ");
+
+// const catBtn = document.querySelector(".get-cat");
+// const foxBtn = document.querySelector(".get-fox");
+// const dogBtn = document.querySelector(".get-dog");
+
+// catBtn.addEventListener("click", getRandomCat);
+// foxBtn.addEventListener("click", getRandomFox);
+// dogBtn.addEventListener("click", getRandomDog);
+
+// // ----------------------------------------------------Get Random Cat Images
+// function getRandomCat() {
+//   fetch("https://aws.random.cat/meow")
+//     .then((response) => response.json())
+//     .then((responseData) => {
+//       cat.innerHTML = `<img src="${responseData.file}" />`;
+//     });
+// }
+
+// // ----------------------------------------------------Get Random Fox Images
+// function getRandomFox() {
+//   fetch("https://randomfox.ca/floof/")
+//     .then((response) => response.json())
+//     .then((responseData) => {
+//       fox.innerHTML = `<img src="${responseData.image}" />`;
+//     });
+// }
+
+// // ----------------------------------------------------Get Random Dog Images
+// function getRandomDog() {
+//   fetch("https://random.dog/woof.json")
+//     .then((response) => response.json())
+//     .then((responseData) => {
+//       dog.innerHTML = `<img src="${responseData.url}" />`;
+//     });
+// }
+
+// give it all you got and reach for the top
+
+/*
+my code below
+*/
+
 let cat = document.querySelector(".cat");
 let fox = document.querySelector(".fox");
-let dog = document.querySelector(".dog ");
+let dog = document.querySelector(".dog");
 
 const catBtn = document.querySelector(".get-cat");
 const foxBtn = document.querySelector(".get-fox");
@@ -15,7 +60,8 @@ catBtn.addEventListener("click", getRandomCat);
 foxBtn.addEventListener("click", getRandomFox);
 dogBtn.addEventListener("click", getRandomDog);
 
-// ----------------------------------------------------Get Random Cat Images
+// kitty cat images function
+
 function getRandomCat() {
   fetch("https://aws.random.cat/meow")
     .then((response) => response.json())
@@ -24,20 +70,23 @@ function getRandomCat() {
     });
 }
 
-// ----------------------------------------------------Get Random Fox Images
+// fleet foxes
+
 function getRandomFox() {
-  fetch("https://randomfox.ca/floof/")
+  fetch("https://randomfox.ca/floof")
     .then((response) => response.json())
     .then((responseData) => {
+      // console.log(responseData);
       fox.innerHTML = `<img src="${responseData.image}" />`;
     });
 }
 
-// ----------------------------------------------------Get Random Dog Images
+// puppydogs
+
 function getRandomDog() {
   fetch("https://random.dog/woof.json")
     .then((response) => response.json())
     .then((responseData) => {
-      dog.innerHTML = `<img src="${responseData.url}" />`;
+      dog.innerHTML = `<img src='${responseData.url}' />`;
     });
 }
